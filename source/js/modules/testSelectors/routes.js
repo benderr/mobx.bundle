8bb2dbfb034cbf1b9c732215810f6844e2563ff1
G.Ilshat <gaifullin.ilshat@gmail.com>
2017-03-28 23:15:29 +0500

    обновил роутинг

--- source/js/modules/testSelectors/routes.js
+++ source/js/modules/testSelectors/routes.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Route, Link} from 'react-router';
 import {TEST_SELECTOR, TEST_SELECTOR_CHILD} from './enums/routes'
 import TransactionsContainer from './containers/TransactionsContainer'
+import InternalLayout from 'components/InternalLayout.jsx'
 
 export function getRoutes() {
 	return (
-		<div>
-			<Route path={ '/' } component={ TransactionsContainer }/>
+		<Route component={InternalLayout}>
+			<Route path={ '/' } />
 			<Route path={ TEST_SELECTOR } component={ TransactionsContainer }/>
 			<Route path={ TEST_SELECTOR_CHILD } component={ TransactionsContainer }/>
-		</div>
+		</Route>
 	);
 }
