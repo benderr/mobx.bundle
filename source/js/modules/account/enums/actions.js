ba582c8d3183be759efae5a31d96a43ee8eac39a
Galiakbarov Timur <T.Galiakbarov@modulbank.ru>
2017-04-17 11:16:35 +0500

    run app saga

--- source/js/modules/account/enums/actions.js
+++ source/js/modules/account/enums/actions.js
@@ -3,4 +3,11 @@ import {createRequestTypes} from 'infrastructure/helpers/actionHelpers'
 export const LOGIN = createRequestTypes('ACCOUNT.LOGIN');
 export const LOGIN_CANCEL = 'ACCOUNT.LOGIN_CANCEL'; //авторизация
 export const LOGOUT = 'ACCOUNT.LOGOUT'; //авторизация
+export const enums = {
+	APP_START: 'APP_START', //запуск приложения
+	LOGIN_INFO: {
+		SUCCESS: 'LOGIN_INFO.SUCCESS',
+		FAIL: 'LOGIN_INFO.FAIL'
+	}
+};
 
