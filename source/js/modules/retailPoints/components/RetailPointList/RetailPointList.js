eef1c02d8ed57e597f2d8d4a84f5e0cd3e835bca
RobertSabiryanov <robert.sabiryanov@gmail.com>
2017-05-25 11:14:52 +0500

    emty retail point list

--- source/js/modules/retailPoints/components/RetailPointList/RetailPointList.js
+++ source/js/modules/retailPoints/components/RetailPointList/RetailPointList.js
@@ -2,32 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 const {arrayOf, string, func} = PropTypes;
 import RetailPointShape from './RetailPointShape';
-import {connect} from 'react-redux';
-import {getRetailPointList, getCurrentRetailPointId} from '../../selectors/retailPointSelectors'
+
 import RetailPointListItem from './RetailPointListItem'
-import {bindActionCreators} from 'redux';
-import * as retailPointActions from '../../actions/retailPointActions';
-import toJs from 'components/HOC/toJs'
 
-const mapActions = dispatch => ({
-	onSelectPoint: bindActionCreators(retailPointActions.setRetailPoint, dispatch)
-});
-
-const mapState = state => ({
-	points: getRetailPointList(state),
-	selectedPointId: getCurrentRetailPointId(state),
-});
-
-@connect(mapState, mapActions)
-@toJs
 class RetailPointList extends React.Component {
 	static propTypes = {
 		points: arrayOf(RetailPointShape),
-		selectedPointId: string
+		selectedPointId: string,
+		onSelectPoint: func
 	};
 
 	render() {
-		const points = this.props.points || [];
+		const {points,selectedPointId,onSelectPoint} = this.props;
 		return (
 
 			<div class='widget_block'>
@@ -44,8 +30,8 @@ class RetailPointList extends React.Component {
 					{points.map(point => (
 						<RetailPointListItem key={'listitem_' + point.id}
 											 point={point}
-											 selectedPointId={this.props.selectedPointId}
-											 onSelectPoint={::this.props.onSelectPoint}
+											 selectedPointId={selectedPointId}
+											 onSelectPoint={onSelectPoint}
 						/>))}
 				</div>
 			</div>
--- source/js/modules/retailPoints/containers/RetailPointsContainer.js
+++ source/js/modules/retailPoints/containers/RetailPointsContainer.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom'
+import {bindActionCreators} from 'redux';
+import toJs from 'components/HOC/toJs'
+import {getRetailPointList, getCurrentRetailPointId} from '../selectors/retailPointSelectors'
 import RetailPointList from '../components/RetailPointList/RetailPointList';
-import { Link } from 'react-router-dom'
+import * as retailPointActions from '../actions/retailPointActions';
 
+
+const mapActions = dispatch => ({
+	onSelectPoint: bindActionCreators(retailPointActions.setRetailPoint, dispatch)
+});
+
+const mapState = state => ({
+	points: getRetailPointList(state),
+	selectedPointId: getCurrentRetailPointId(state),
+});
+
+@connect(mapState, mapActions)
+@toJs
 class RetailPointsContainer extends React.Component {
 	render() {
-		return (<div>
+		const { points, selectedPointId, onSelectPoint} = this.props;
+		return ( points && points.length > 0 ? <div>
 			<div class="title_panel">
 				<h1>Точки продаж</h1>
 				<div class="title_actions">
 					<Link class="button small icon-plus" to="/retail-points/add">Добавить точку</Link>
 				</div>
 			</div>
-			<RetailPointList />
+			<RetailPointList points={points} selectedPointId={selectedPointId} onSelectPoint={onSelectPoint}/>
+		</div> : <div class="pos_0">
+			<div class="pos_0_inner">
+				<i class="icon-pos"></i>
+				<p style={{fontSize: '18px'}}>У вас еще нет торговых точек</p>
+				<p style={{fontSize: '14px'}}>Для добавления товаров необходима добавить точку продаж</p>
+				<Link class="button small icon-plus mt28" to="/retail-points/add">Добавить точку продаж</Link>
+			</div>
 		</div>);
 	}
 }
