e71e7ddb57fd63977fbbf45f31777ed09edfa88a
RobertSabiryanov <robert.sabiryanov@gmail.com>
2017-05-25 10:24:09 +0500

    retail point adding

--- source/js/modules/retailPoints/components/RetailPointList/RetailPointList.js
+++ source/js/modules/retailPoints/components/RetailPointList/RetailPointList.js
@@ -31,7 +31,6 @@ class RetailPointList extends React.Component {
 		return (
 
 			<div class='widget_block'>
-				{/*{this.props.selectedPointId}*/}
 				<div class='table  table_pos'>
 					<div class='table_head'>
 						<div class='pos_name'>Название</div>
@@ -39,6 +38,9 @@ class RetailPointList extends React.Component {
 						<div class="pos_amount">Сумма продаж</div>
 						<div class="pos_action">Действия</div>
 					</div>
+					{/*<div class="table_row  row_link_search">*/}
+						{/*<input type="search" class="small  w100" placeholder="Поиск по точкам продаж"/>*/}
+					{/*</div>*/}
 					{points.map(point => (
 						<RetailPointListItem key={'listitem_' + point.id}
 											 point={point}
--- source/js/modules/retailPoints/components/RetailPointList/RetailPointListItem.js
+++ source/js/modules/retailPoints/components/RetailPointList/RetailPointListItem.js
@@ -6,9 +6,11 @@ const RetailPointListItem = ({point, selectedPointId, onSelectPoint}) => {
 	return (<div class='table_row  row_link'>
 		<div class='pos_name'>{point.name}</div>
 		<div class='pos_address'>{point.address} {point.settings.aboutModulPosUrl}</div>
-		<div class='pos_amount'>103 549.00 р. </div>
+		<div class='pos_amount'>Н/Д</div>
 		<div class='pos_action'>
-			<button disabled={selectedPointId == point.id} className="xsmall second button" onClick={()=>onSelectPoint(point.id)}>Выбрать</button>
+			<input type="checkbox" name={point.id} id={point.id} checked={selectedPointId == point.id}
+				   onChange={() => onSelectPoint(point.id)}/>
+			<label for={point.id} class="label_check switcher small"><i class="icon"></i><span>Текущая</span></label>
 		</div>
 	</div>);
 };
