dcf594d55957edcc0a646cc9f4b24fc0b58ee83b
RobertSabiryanov <robert.sabiryanov@gmail.com>
2017-06-12 20:12:50 +0500

    edit points

--- source/js/modules/retailPoints/components/RetailPointList/RetailPointListItem.js
+++ source/js/modules/retailPoints/components/RetailPointList/RetailPointListItem.js
@@ -5,8 +5,8 @@ import {string, func} from 'prop-types';
 const RetailPointListItem = ({point, selectedPointId, onSelectPoint, onItemClick}) => {
 	return (<div class='table_row  row_link' >
 		<div class='pos_name'  onClick={() => onItemClick(point.id)}>{point.name}</div>
-		<div class='pos_address'>{point.address} {point.settings.aboutModulPosUrl}</div>
-		<div class='pos_amount'>Н/Д</div>
+		<div class='pos_address' onClick={() => onItemClick(point.id)}>{point.address} {point.settings.aboutModulPosUrl}</div>
+		<div class='pos_amount' onClick={() => onItemClick(point.id)}>Н/Д</div>
 		<div class='pos_action'>
 			<input type="checkbox" name={point.id} id={point.id} checked={selectedPointId == point.id}
 				   onChange={() => onSelectPoint(point.id)}/>
