65941cb45fd7f5f523397ff2fda87e62133fd3f4
RobertSabiryanov <robert.sabiryanov@gmail.com>
2017-06-19 00:15:19 +0500

    1

--- source/js/modules/retailPoints/selectors/retailPointSelectors.js
+++ source/js/modules/retailPoints/selectors/retailPointSelectors.js
@@ -12,14 +12,6 @@ export const getCurrentRetailPointId = createSelector([getRetailPointsData], rpD
 	return rpData.get('selectedPointId');
 });
 
-// export const getCurrentRetailPoint = createSelector([getRetailPointsData], rpData => {
-// 	const points = rpData.get('retailPoints');
-// 	if (!points)
-// 		return null;
-// 	const index = points.findIndex(item => item.get('id') === getCurrentRetailPointId());
-// 	return points.get(index);
-// });
-
 export const getRetailPointInLayer = createSelector([getRetailPointsData], rpData => {
 	return rpData.getIn(['retailPointInLayer'], null);
 });
