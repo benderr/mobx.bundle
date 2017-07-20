33858d491589eda108b4e3cf69adcbd77f66c900
RobertSabiryanov <robert.sabiryanov@gmail.com>
2017-07-20 14:43:55 +0500

    tests

--- source/js/modules/products/selectors/__mocks__/productsSelectors.js
+++ source/js/modules/products/selectors/__mocks__/productsSelectors.js
@@ -1,15 +1,15 @@
 /**
  * Created by RobertSabiryanov on 18.07.17.
  */
-import {Map, List, fromJS} from 'immutable';
+import {fromJS} from 'immutable';
 
 const productsSelectors = jest.genMockFromModule('modules/products/selectors/productsSelectors');
 
 productsSelectors.getProductDetailSection= ()=>{
-	return Map({
-		productView: Map({}),
-		searchProductsResult: Map({}), //результаты поиска в выпадушке
-		searchGroupsResult: Map({}) //результаты поиска группы модификаторов в выпадушке
+	return fromJS({
+		productView: {},
+		searchProductsResult: {}, //результаты поиска в выпадушке
+		searchGroupsResult: {} //результаты поиска группы модификаторов в выпадушке
 	})
 }
 module.exports = productsSelectors;
