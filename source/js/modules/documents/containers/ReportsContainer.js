c8d6a9087e048dac03a71868eff985a8e7803db5
A.Buylov <buylovav@modulbank.ru>
2017-08-01 09:01:13 +0500

    f

--- source/js/modules/documents/containers/ReportsContainer.js
+++ source/js/modules/documents/containers/ReportsContainer.js
@@ -6,7 +6,10 @@ import toJS from 'components/HOC/toJs';
 import retailPointHOC from 'components/HOC/retailPointRequiredHOC';
 import TitlePanel from '../components/TitlePanel'
 import ReportForm from '../components/ReportFormComponent'
+import {getCurrentRetailPointId} from 'modules/retailPoints/selectors/retailPointSelectors'
+import moment from 'moment'
 
+import {getSection} from 'modules/account/selectors/accountSelectors'
 import * as selectors from '../selectors/reportSelectors'
 import * as actions from '../actions/reportActions'
 
@@ -18,10 +21,38 @@ import * as actions from '../actions/reportActions'
 class ReportsContainer extends React.Component {
 
 	onSubmitForm(props) {
+		const {reportState, token} = this.props;
+
+		if (!reportState.isFromEmail) {	// скачать отчет
+			const t = getCurrentRetailPointId;
+			console.log('t', t);
+
+
+			// const beginDate = moment(props.get('beginDate'), "DD.MM.YYYY").format();
+			// const endDate = moment(props.get('endDate'), "DD.MM.YYYY").format();
+            //
+			// const [protocol, _, host] = window.location.href.split("/").slice(0, 3);
+			// const downloadLink = document.createElement("a");
+			// const values = atob(token).split(':');
+			// const email = values[0];
+			// const password = values[1];
+            //
+			// // GET /v1/retail-point/{retailPointId}/downloadSalesReport
+			// downloadLink.href = `/api/v1/retail-point/${token}/downloadSalesReport?beginDate=${beginDate}&endDate=${endDate}`;
+			// downloadLink.download = "report.xls";
+            //
+			// console.log(downloadLink);
+            //
+			// document.body.appendChild(downloadLink);
+			// downloadLink.click();
+			// document.body.removeChild(downloadLink);
+
+		} else {						// отправить на EMail
+
+		}
+
+
 		const {salesReportAction} = this.props;
-
-		console.log(props.toJS());
-
 		salesReportAction({
 			beginDate: props.get('beginDate'),
 			endDate: props.get('endDate'),
@@ -51,7 +82,9 @@ class ReportsContainer extends React.Component {
 
 function mapStateToProps(state, ownProps) {
 	const reportState = selectors.getReport(state);
-	return {reportState};
+	const token = getSection(state).get('token');
+
+	return {reportState, token};
 }
 
 function mapDispatchToProps(dispatch) {
--- source/js/modules/documents/sagas/reportSagas.js
+++ source/js/modules/documents/sagas/reportSagas.js
@@ -8,12 +8,22 @@ import * as enums from '../actions/reportActions'
 
 function* salesReportSaga({beginDate, endDate, fromEmail}) {
 	try {
-		beginDate = moment(beginDate, "DD.MM.YYYY").format();
-		endDate = moment(endDate, "DD.MM.YYYY").format();
+		beginDate = moment(beginDate, "DD.MM.YYYY").format('YYYY-MM-DD');
+		endDate = moment(endDate, "DD.MM.YYYY").format('YYYY-MM-DD');
 
 		const token = yield select(getCurrentRetailPointId);
-		const data = yield call(dataContext.getDownloadSalesReport, token, beginDate, endDate);
 
+		const downloadLink = document.createElement("a");
+		downloadLink.href = `/api/v1/retail-point/${token}/downloadSalesReport?beginDate=${beginDate}&endDate=${endDate}`;
+		//downloadLink.download = "report.xls";
+		downloadLink.target = "_blank";
+		document.body.appendChild(downloadLink);
+		downloadLink.click();
+		// document.body.removeChild(downloadLink);
+
+		console.log(downloadLink);
+
+		const data = yield call(dataContext.getDownloadSalesReport, token, beginDate, endDate);
 		console.log('result saga', data);
 	} catch (error) {
 		console.log(error);
