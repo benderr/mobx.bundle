4abe44628d7496252f231829dda8983472d41664
A.Buylov <buylovav@modulbank.ru>
2017-08-03 15:59:25 +0500

    При вводе НЕ ЧИСЛА падает NaN -> parseFloat('q') = NaN

--- source/js/common/formElements/fields/parseNumber.js
+++ source/js/common/formElements/fields/parseNumber.js
@@ -1,6 +1,8 @@
 export default (value) => {
 	if (!value)
 		return value;
+	if (!(+value))
+		return '';
 	if (value.replace)
 		value = value.replace(/[^0-9\.,]+/g, '').replace(',', '.');
 	return parseFloat(value);
