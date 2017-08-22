8dd4286db1561e3788983c537444b411bfe2ef5b
alexandr.buylov <buylovav@modulbank.ru>
2017-08-22 20:57:59 +0500

    MK-1576

--- source/js/modules/account/containers/ForgotContainer.js
+++ source/js/modules/account/containers/ForgotContainer.js
@@ -10,12 +10,62 @@ import ModulHeader from 'components/ModulHeader';
 
 class ForgotContainer extends React.Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			countError: 0,
+			showCaptcha: false,
+			captcha: null,
+			captchaReady: false
+		};
+	}
+
+	componentWillReceiveProps(props) {
+		const {errors} = props;
+
+		let stateUpdate = {
+			countError: this.state.countError,
+			showCaptcha: this.state.showCaptcha
+		};
+
+		if (errors && errors.status && errors.status === 415) {
+			stateUpdate.countError++;
+			if (stateUpdate.countError > 3) {
+				stateUpdate.showCaptcha = true;
+				stateUpdate.captchaReady = false;
+				stateUpdate.countError = 0;
+			} else {
+				stateUpdate.showCaptcha = false;
+			}
+		}
+
+		this.setState(stateUpdate);
+	}
+
 	handleSendEmail(form) {
+		if (this.state.showCaptcha) {
+			if (!this.state.captcha)
+				return false;
+		}
+
 		this.props.sendMail(form.get('email'));
 	}
 
 	componentWillUnmount() {
 		this.props.resetForm();
+	}
+
+	onCaptchaChange(value) {
+		this.setState({
+			captcha: value
+		});
+	}
+
+	onCaptchaLoad() {
+		this.setState({
+			captchaReady: true
+		});
 	}
 
 	render() {
@@ -30,6 +80,12 @@ class ForgotContainer extends React.Component {
 									loading={loading}
 									isSent={success}
 									errors={errors}
+
+									showCaptcha={this.state.showCaptcha}
+									captcha={this.state.captcha}
+									captchaReady={this.state.captchaReady}
+									onCaptchaChange={::this.onCaptchaChange}
+									onCaptchaLoad={::this.onCaptchaLoad}
 						/>
 						<div className="login_links">
 							<Link to="/signin">Войти</Link>
