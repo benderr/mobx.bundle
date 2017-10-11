dffa05e75034bfccb9d06e3e97111bc43d82ec8b
dev <zakharov.dy@mail.ru>
2017-10-11 19:11:18 +0500

    eslint config в readme для webstorm

--- source/js/components/InternalLayout.jsx
+++ source/js/components/InternalLayout.jsx
@@ -3,19 +3,19 @@ import SiteHeader from 'components/SiteHeader';
 
 
 class InternalLayout extends React.Component {
-    render() {
-        return (
-            <div class="poss">
-                <SiteHeader/>
-                <section class="main">
-                    <div class="section_content full_width" style={{minHeight: '700px'}}>
-                        {this.props.children}
-                    </div>
-                </section>
-            </div>
+  render() {
+    return (
+      <div class='poss'>
+        <SiteHeader />
+        <section class='main'>
+          <div class='section_content full_width' style={ { minHeight: '700px' } }>
+            {this.props.children}
+          </div>
+        </section>
+      </div>
 
-        );
-    }
+    );
+  }
 }
 
 export default InternalLayout;
